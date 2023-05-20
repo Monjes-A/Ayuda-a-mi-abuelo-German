cb4714f21577b29c8455335d4b1acad70858a9ad
Monjez <Jesusjesusmonsalve1414@gmail.com>
2023-05-20 19:05:53 -0400

    creando barra de progreso

---
+++ assets/css/main.js
@@ -0,0 +1,15 @@
+// Porcentaje actual de la recaudación
+var currentAmount = 30; // Actualiza este valor según el progreso real
+
+// Monto objetivo
+var targetAmount = 8000;
+
+// Calcula el porcentaje
+var percentage = (currentAmount / targetAmount) * 100;
+
+// Actualiza la barra de carga y el texto
+var progressBar = document.getElementById("progress");
+progressBar.style.width = percentage + "%";
+
+var progressText = document.getElementById("progress-text");
+progressText.innerText = percentage.toFixed(2) + "%";
